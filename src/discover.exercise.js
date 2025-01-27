361d740b89ee3b45240bfe1d0c7a0d01533c4179
Rayana Sales <rayanaasales@gmail.com>
2025-01-26 22:42:35 -0300

    making http requests - exercise 01 and 02

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -1,36 +1,35 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
-
+import React from 'react'
 import './bootstrap'
 import Tooltip from '@reach/tooltip'
 import {FaSearch} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import {client} from './utils/api-client'
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
+  const [status, setStatus] = React.useState('idle')
+  const [query, setQuery] = React.useState('')
+  const [queried, setQueried] = React.useState(false)
+  const [data, setData] = React.useState(null)
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
+  const isLoading = status === 'loading'
+  const isSuccess = status === 'success'
 
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  React.useEffect(() => {
+    if (!queried) return
+    setStatus('loading')
+    client(`books?query=${encodeURI(query)}`).then(responseData => {
+      setData(responseData)
+      setStatus('success')
+    })
+  }, [query, queried])
 
   function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
-    // 🐨 set the queried state to true
-    // 🐨 set the query value which you can get from event.target.elements
-    // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault()
+    setQuery(event.target.elements.search.value)
+    setQueried(true)
   }
 
   return (
