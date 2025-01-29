529f9951b67e6fa7bc3e80bb326504c7f93b92d0
Rayana Sales <rayanaasales@gmail.com>
2025-01-28 21:03:03 -0300

    Exercise 04: retriving user from api auth and set logged session

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,11 +3,28 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {client} from './utils/api-client'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
 function App() {
-  const [user, setUser] = React.useState(null)
+  const [user, setUser] = React.useState(getUser())
+
+  React.useEffect(() => {
+    getUser().then(u => setUser(u))
+  }, [])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
